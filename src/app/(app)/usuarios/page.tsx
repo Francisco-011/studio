d96e9c1cfabd1d9bb9d23de3bdfbbd5b315a56d2
Francisco Rivera <franciscorivera6114@gmail.com>
2025-06-20 21:45:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/usuarios/page.tsx
+++ src/app/(app)/usuarios/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useEffect, type ReactNode } from 'react';
+import { useState, useEffect, useMemo, type ReactNode } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
