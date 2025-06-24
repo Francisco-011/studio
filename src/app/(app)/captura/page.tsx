5439a3a8c188273b7d1b7e97518d26c09b9fe2a6
Francisco Rivera <franciscorivera6114@gmail.com>
2025-06-24 17:42:30 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/captura/page.tsx
+++ src/app/(app)/captura/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useEffect, useMemo, useRef } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
